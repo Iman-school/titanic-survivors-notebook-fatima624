27feb0f54e1972b2af81b9f9e5d69d504437e962
fatima624 <99713814+fatima624@users.noreply.github.com>
2022-03-05 16:01:33 +0300

    Add files via upload

---
+++ l4 final evaluation (1).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as rp
+import pandas as pd
+
+
+# In[5]:
+
+
+df= pd.read_csv('titanic-passengers.csv', sep=';')
+df.head()
+
+
+# In[ ]:
+
+
+
+
